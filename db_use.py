7c2a5d6e70aab7545335f1ae6a80d8e32aa3c60c
marutaro1 <87046415+marutaro1@users.noreply.github.com>
2023-05-08 12:42:27 +0900

    Update db_use.py

--- db_use.py
+++ db_use.py
@@ -45,8 +45,6 @@ connection = MySQLdb.connect(
 cursor = connection.cursor()
 cursor.execute('set global wait_timeout=86400')
 
-switch_motor = motor.ServoMotor()
-
 states = ['go', 'return','go_record','return_record','post_go_record','post_return_record']
 transitions = [
 	{'trigger':'go','source':'go', 'dest':'go_record'},#goの信号を受け取る
@@ -58,10 +56,11 @@ transitions = [
 class SwitchDB(object):
 	def __init__(self):
 	    #self.page_value = input('go or return')
+	    self.now = datetime.datetime.now()
+	    self.day = str(self.now)[0:11]
 	    self.page_value = cr.card_type
 	    self.idm = ''
 	    self.backup = []
-	    self.check_record_time = ''
 	    
 	#日時、名前、詳細をslackに通知させる
 	def notification(day,time,name,nb):
@@ -70,7 +69,7 @@ class SwitchDB(object):
 		    return
 	    url = "https://slack.com/api/chat.postMessage"
 	    data = {
-	    "token":os.environ['SLACK_TOKEN'],
+	    "token":'xoxb-4610993849044-4611014137156-2pTJZhzzP48hBDYARzOyvYf7',
 	    "channel":"exitresident",
 	    "text":"%s %s %s様: %s" % (day,time,name,nb)
 	    }
@@ -82,6 +81,7 @@ class SwitchDB(object):
 	    
 	#idを選択し、card_recordとresidentを合わせて一致する最新のデータを1つ呼び出す
 	def select_card_record(self,day,cr):
+	    print('add_door')
 	    cursor.execute("""SELECT
 			     resident.id,
 			     resident.name,
@@ -105,12 +105,14 @@ class SwitchDB(object):
 			     card_record.idm = '%s'
 			     ORDER BY card_record.datetime DESC
 			     """ % ('一人外出可%',day + '%',self.page_value,cr.idm_data))
-	    if cursor.fetchone() is not None and cr.motor_run == 'ok':
-		    switch_motor.move_to_position(30)
-	    return cursor.fetchone()
+	    print('card_record')
+	    card_record = cursor.fetchone()
+	    print(card_record)
+	    return card_record
 	    
 	#日付とresident_idが一致するdoor_recordの最新のデータを一つ呼び出す
 	def select_door_record(day,resident_id):
+	    
 	    cursor.execute("""SELECT
 		    resident_id,
 		    exit_day,
@@ -128,37 +130,32 @@ class SwitchDB(object):
 	
 	##goかreturnかの信号を受け取り、door_recordを登録する
 	def add_door_record(event):
+	   
 	    check_time = event.kwargs.get('check_time')
 	    resident_id = event.kwargs.get('resident_id')
 	    resident_nb = event.kwargs.get('resident_nb')
 	    page_value = event.kwargs.get('page_value')
 	    day = event.kwargs.get('day')
 	    time = event.kwargs.get('time')
-	    cursor.execute("SELECT * FROM door_record ORDER BY id DESC")
-	    last_data = cursor.fetchone()
+	    print('time' + time)
 	    resident = SwitchDB.select_resident(resident_id)
-	    time_obj = datetime.datetime.strptime(str(last_data[3]), '%H:%M:%S')
-	    last_data_time = time_obj.strftime('%H:%M:%S')
-	    print(str(last_data[2]) + ' ' + last_data_time)
-	    print(check_time)
-	    if (str(last_data[2]) + ' ' + last_data_time) != str(check_time):
-		    print('log True')
-		    door_state = ['exit_day','exit_time']
-		    judgment = event.kwargs.get('judgment')
-		    if page_value == 'return':
-			    door_state = ['entrance_day','entrance_time']
-			    day_record = SwitchDB.select_door_record(day,resident_id)
-			    print(day_record)
-			    if day_record is not None and day_record[3] is None:
-				    print('return update')
-				    cursor.execute(f"update door_record set entrance_day=%s,entrance_time=%s,nb='%s' where exit_day = %s and exit_time <= %s and resident_id = %s order by exit_time desc limit 1",(day,time,resident_nb,day,time,resident_id))
-				    connection.commit()
-				    return
-		    print('puls add')
-		    cursor.execute(f"insert into door_record (resident_id,%s,%s,nb,error_judgment) values (%s,'%s','%s','%s','%s')" % (door_state[0],door_state[1],resident_id,day,time,resident_nb,judgment))
-		    connection.commit()
-		    SwitchDB.check_record_time = check_time
-		    SwitchDB.notification(day,time,resident[1],resident_nb) 
+	    
+	    print('log True')
+	    door_state = ['exit_day','exit_time']
+	    judgment = event.kwargs.get('judgment')
+	    if page_value == 'return':
+		    door_state = ['entrance_day','entrance_time']
+		    day_record = SwitchDB.select_door_record(day,resident_id)
+		    print(day_record)
+		    if day_record is not None and day_record[3] is None:
+			    print('return update')
+			    cursor.execute(f"update door_record set entrance_day=%s,entrance_time=%s,nb='%s' where exit_day = %s and exit_time <= %s and resident_id = %s order by exit_time desc limit 1",(day,time,resident_nb,day,time,resident_id))
+			    connection.commit()
+			    return
+	    print('puls add')
+	    cursor.execute(f"insert into door_record (resident_id,%s,%s,nb,error_judgment) values (%s,'%s','%s','%s','%s')" % (door_state[0],door_state[1],resident_id,day,time,resident_nb,judgment))
+	    connection.commit()
+	    SwitchDB.notification(day,time,resident[1],resident_nb) 
 	     
 	def csv_migrate(self):
 	     cursor.execute("""
@@ -183,21 +180,24 @@ class SwitchDB(object):
 		    day = str(now)[0:11]
 		    print(cr.card_data())
 		    self.idm = cr.idm_data
+		    print('new_record')
 		    new_record = self.select_card_record(day,cr)
 		    print(new_record)
-		    if new_record is None:
-			    print('pass')
-			    return
+		    print(new_record[3])
 		    
-		    print('switch')
-		    #response = requests.post('https://api.switch-bot.com/v1.0/devices/FA9364B2BC98/commands',headers=headers,json=json_data)
-		    machine = Machine(model=SwitchDB, states=states, transitions=transitions, initial=self.page_value,
-		    auto_transitions=False, ordered_transitions=False,send_event=True)
-		    SwitchDB.trigger(self.page_value)
-		    SwitchDB.trigger(self.state,check_time=new_record[3],resident_id=new_record[0],resident_nb=new_record[2],page_value=self.page_value,day=str(new_record[3])[0:11],time=str(new_record[3])[11:19],judgment=judgment)
-		    
-		    print('error: ' + cr.error_judgment)
-		    connection.commit()
+		    if new_record is not None and cr.motor_run == 'ok':
+			    switch_motor.move_to_position(30)
+			    
+			    print('switch')
+			    #response = requests.post('https://api.switch-bot.com/v1.0/devices/FA9364B2BC98/commands',headers=headers,json=json_data)
+			    machine = Machine(model=SwitchDB, states=states, transitions=transitions, initial=self.page_value,
+			    auto_transitions=False, ordered_transitions=False,send_event=True)
+			    SwitchDB.trigger(self.page_value)
+			    SwitchDB.trigger(self.state,check_time=new_record[3],resident_id=new_record[0],resident_nb=new_record[2],page_value=self.page_value,day=str(new_record[3])[0:11],time=str(new_record[3])[11:19],judgment=judgment)
+			   
+			    print('error: ' + cr.error_judgment)
+			    
+			    
 					    
 	    except MySQLdb.OperationalError as e:
 		    print(e)
@@ -205,10 +205,20 @@ class SwitchDB(object):
 switch_db = SwitchDB()
 while True:
     try:
-	    response = requests.get('http://192.168.0.31')#ネットワーク確認用
+	    response = ''
+	    #requests.get('http://192.168.0.31')#ネットワーク確認用
+	    
 	    switch_db.mb('no')
-    
+	    new_record = switch_db.select_card_record(switch_db.day,cr)
+	    print(new_record)
+	    print(new_record[3])
+	    if switch_db.page_value == 'go':
+		    cursor.execute(f"UPDATE door_record SET exit_time = '%s' ORDER BY exit_time DESC LIMIT 1" % (str(new_record[3])[11:19]))
+		    connection.commit()
+	   
     except requests.exceptions.ConnectionError as e:
 	    print(e)
 	    switch_db.mb('error')
+    
+
     
