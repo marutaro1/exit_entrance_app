84dbc3f592cf0075ed85a82d911482684a3487be
marutaro1 <87046415+marutaro1@users.noreply.github.com>
2023-07-04 19:33:58 +0900

    Update switch_app.py

--- switch_app.py
+++ switch_app.py
@@ -467,7 +467,7 @@ class SwitchView(object):
 		# 停止したいプロセス名を指定する
 		process_name = "db_use.py"
 		print('kill db_use')
-		os.system(f'pkill -f {process_name}')
+		os.system(f'sudo pkill -f {process_name}')
 	
 	#変更後
 	def restart_db_use():
